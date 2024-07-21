73104e656d2e64a77713d48b196b9c476e4a3a6a
Sam Nunn <sam@nunn.io>
2024-07-22 07:59:35 +0800

    Production updates

--- app/main.js
+++ app/main.js
@@ -111,6 +111,26 @@ for (let i of [weightInput, heightInput]) {
 
 bmiOutput.addEventListener('input', (e) => {
     let stopBangBMI = document.querySelector('#stopbang-bmi')
+    if (parseFloat(e.target.value) > 35) {
+        stopBangBMI.checked = true
+    } else {
+        stopBangBMI.checked = false
+    }
+})
+
+let sexInput = document.querySelector('#anthropometry-sex')
+let apfelSexOutput = document.querySelector('#apfel-sex')
+let stopbangSexOutput = document.querySelector('#stopbang-sex')
+sexInput.addEventListener('input', (e) => {
+    if (sexInput.value == 'm') {
+        apfelSexOutput.checked = false
+        stopbangSexOutput.checked = true
+    } else {
+        apfelSexOutput.checked = true
+        stopbangSexOutput.checked = false
+    }
+    apfelSexOutput.dispatchEvent(new Event('input'))
+    stopbangSexOutput.dispatchEvent(new Event('input'))
 })
 
 // DOT POINTS
@@ -142,6 +162,7 @@ let deepLinkTemplates = {
     'inteleconnect':   'https://inteleconnect.hdwa.health.wa.gov.au/Portal/app#patients/A1234567',
 }
 let umrnInput = document.getElementById('umrn')
+let deeplinksButton = document.querySelector('#deeplinks')
 umrnInput.addEventListener('input', (e) => {
     let umrnTester = /^[A-Za-z]\d{7}$/
     let valid = umrnTester.test(e.target.value)
@@ -149,13 +170,110 @@ umrnInput.addEventListener('input', (e) => {
         for (let t in deepLinkTemplates) {
             let targetLink = document.querySelector(`[deeplink="${t}"]`)
             let href = deepLinkTemplates[t].replace('A1234567', e.target.value)
+            targetLink.setAttribute('target', "_blank")
             targetLink.setAttribute('href', href)
+            targetLink.setAttribute('aria-disabled', 'false')
+            targetLink.style = 'pointer-events: auto;'
+            deeplinksButton.classList.add('contrast')
+            deeplinksButton.classList.remove('secondary')
+            deeplinksButton.classList.remove('outline')
         }
     } else {
         let deeplinks = document.querySelectorAll('[deeplink]')
         for (let d of deeplinks) {
-            d.setAttribute('disabled', true)
-            d.setAttribute('href', '#')
+            d.setAttribute('aria-disabled', 'true')
+            d.removeAttribute('href')
+            deeplinksButton.classList.add('secondary')
+            deeplinksButton.classList.add('outline')
+            deeplinksButton.classList.remove('contrast')
         }
     }
+})
+
+// TEMPLATE
+
+const template = `# BIO
+- Name: {{ biography-name }}
+- UMRN: {{ biography-umrn }}
+
+# ANTHROPOMETRY
+- Age: {{ anthropometry-age }}
+- Sex: {{ anthropometry-sex }}
+- Height: {{ anthropometry-height }}
+- Weight: {{ anthropometry-weight }}
+- BMI: {{ anthropometry-bmi }}
+
+# PMHx
+{{ history-pmhx }}
+ 
+# SHx
+{{ history-socialhx }}
+ 
+# Medications
+{{ history-rx }}
+ 
+# Allergies
+{{ history-allergies }}
+ 
+# PSHx
+{{ history-pshx }}
+ 
+# Anaesthetic Assessment
+{{ history-examination }}
+ 
+- STOPBANG: {{ score-stopbang }}/8
+- RCRI: {{ score-rcri }}/6
+- Apfel: {{ score-apfel }}/4
+- SORT: {{ score-sort }}
+
+# CONSENT
+Discussed risks and benefits of GA by prevalence.  
+
+- VERY COMMON: sore throat (45% ETT, 20% LMA), PONV
+- COMMON: minor lip/tongue injury (1 in 100)
+- RARE: damage to teeth, severe allergy, nerve damage
+- VERY RARE: awareness, death (1 in 100,000 ASA 1, 1 in 50,000 for all ASAs)
+
+Specific risks including aspiration, LRTI, post op confusion, covert CVA with possible cognitive changes, temporary memory loss, myocardial infarction also discussed.
+
+Consented for sedation, with risks discussed including death, failure, allergy, awareness, pain and need to progress to GA with its associated risks.
+
+Regional risks discussed - superficial skin infection, bleeding, nerve damage (parasthesia and/or paralysis), failure of block, damage to surrounding structures, adverse drug reactions 
+
+Discussed risks and benefits of spinal anaesthesia. Specifically, nausea and vomiting, backache, headache, prolonged numbness or tingling, hypotension, urinary retention, nerve injury (1 in 500 temporary, ~1 in 25,000 permanent) and failure of regional technique
+
+Consented to blood products.
+
+# FASTING ADVICE
+Fasting from midnight if AM, fasting from early morning, light breakfast if PM list.
+`
+
+function renderTemplate() {
+    let output = template
+    let tagFinder = /\{\{ ?(.*\b) ?\}\}/gim
+    for (let m of output.matchAll(tagFinder)) {
+        // find in DOM
+        let [stringtoReplace, paasSyncParameter] = m
+        let paasSyncTarget = document.querySelector(`[paas-sync-parameter="${paasSyncParameter}"]`)
+
+        // replace (if it exists)
+        let paasSyncValue = ''
+        if (!paasSyncTarget) {
+            console.warn(`TEMPLATE ERROR: no such parameter as "${paasSyncParameter}"`)
+        } else {
+            paasSyncValue = paasSyncTarget.value || ''
+        }
+
+        output = output.replace(stringtoReplace, paasSyncValue)
+
+    }
+    return output
+}
+
+let renderButton = document.querySelector('#render')
+let outputArea = document.querySelector('#output')
+let dialog = document.querySelector('dialog')
+renderButton.addEventListener('click', (e) => {
+    outputArea.innerText = renderTemplate()
+    dialog.show()
 })
