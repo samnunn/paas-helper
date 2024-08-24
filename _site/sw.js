50e1689f35c9d11e8edabe7b8a6c8c17e116c202
Sam Nunn <sam@nunn.io>
2024-08-24 20:59:08 +0800

    Add a basic service worker

---
+++ _site/sw.js
@@ -0,0 +1,77 @@
+let version = "1"
+
+let cacheName = `v${version}_data`
+
+let cachedAssetPaths = [
+    './',
+    'index.html',
+    'main.css',
+    'main.js',
+    'chevrons-down.svg',
+    'oplist.json',
+    'favicon.ico',
+]
+
+// install (pre-activation) event
+    // waitUntil() accepts a Promise and *waits* for it to resolve
+    // alt title: waitForResolutionOfThisPromise(<Promise>)
+self.addEventListener('install', event => {
+    self.skipWaiting()
+    event.waitUntil(install())
+})
+
+async function install() {
+    console.log(`SW ${version}: installing`)
+
+    let cache = await caches.open(cacheName)
+
+    for (let p of cachedAssetPaths) {
+        try {
+            await cache.add(p)
+            console.log(`SW ${version}: pre-downloaded asset @ ${p}`)
+        } catch ({name, message}) {
+            console.error(`SW ${version}: failed to pre-download asset @ ${p}`)
+        } finally {
+            continue
+        }
+    }
+}
+
+// activation (post-installation) event
+self.addEventListener('activate', (e) => {
+    e.waitUntil(activate())
+})
+
+async function activate() {
+    console.log(`SW ${version}: activating`)
+    let allCaches = await caches.keys()
+    let badCaches = allCaches.filter((key) => { return key != cacheName })
+    for (let c of badCaches) {
+        caches.delete(c)
+    }
+    await self.clients.claim()
+}
+
+// fetch (every request)
+self.addEventListener('fetch', async (e) => {
+    // https://stackoverflow.com/a/49719964
+    // via https://gomakethings.com/toolkit/boilerplates/service-worker/
+    if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
+
+    e.respondWith(cacheBeforeNetwork(e.request))
+})
+
+// matching methods
+async function cacheBeforeNetwork(request) {
+    console.log(`SW ${version}: running cacheBeforeNetwork for ${request.url}`)
+
+    // match and return
+    let match = await caches.match(request)
+    if (match) {
+        console.log(`SW ${version}: ${request.url} served from cache 💾`)
+        return match
+    }
+    // fall back to network
+    console.log(`SW ${version}: ${request.url} served from network 🛜`)
+    return await fetch(request)
+}
